8555a3d1192350faf3a6cc81c4d3edfd8d51fcdd
ayelenleclerc <ayelenleclerc@gmail.com>
2021-06-14 17:50:32 -0300

    probando desde atom

---
+++ javascript/main.js
@@ -0,0 +1,33 @@
+// Obtener la clase close
+
+  let link= document.querySelectorAll(".close")
+// recorrerlos
+
+  link.forEach(function(link){
+
+
+//agregar un evento click a cada uno de ellos
+
+    link.addEventListener("click" , function(ev){
+      ev.preventDefault();
+      let content = document.querySelector(".content")
+
+
+      // Vamos a quitarle a content las clases de animacion
+
+      content.classList.remove("animate__fadeInDown")
+      content.classList.remove("animate__animated")
+
+
+      //Vamos a agregar clases para animar su salida FadeOutUp
+
+      content.classList.add("animate__fadeOutUp")
+      content.classList.add("animate__animated")
+
+      setTimeout(function(){
+        location.href = "/";
+      } , 600);
+      })
+
+    });
+//
